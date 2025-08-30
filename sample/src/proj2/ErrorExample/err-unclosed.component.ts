4ac1e7b3d4217b6f7659cdfe222a6c7a2f3f6e92
Aziz7172 <azizali7172@gmail.com>
2025-08-30 10:39:21 +0300

    Add files via upload

---
+++ sample/src/proj2/ErrorExample/err-unclosed.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-err-unclosed',
+  templateUrl: './err-unclosed.component.html'
+})
+export class ErrUnclosedComponent {
+  title: string = 'Test';
+}
